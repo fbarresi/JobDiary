796fb3fe3e2e1b3e2c005a6d514ca7b4508a2669
fbarresi <fede.barresi@gmail.com>
2018-12-30 00:36:05 +0100

    implemented function start and stop

--- jobdiary/core.py
+++ jobdiary/core.py
@@ -1,7 +1,15 @@
 import sys
+from pathlib import Path
+from tinydb import TinyDB, Query
+from huepy import *
+import datetime
 
 
 from jobdiary.texts import usage
+
+
+db = TinyDB(str(Path.home().joinpath("jobdiary.json")))
+
 
 def decode_args(args, encoding=None):
     """
@@ -14,6 +22,27 @@ def decode_args(args, encoding=None):
         for arg in args
     ]
 
+def start():
+    now = datetime.datetime.now()
+    entry = Query()
+    result = db.search(entry.day == str(now.date()))
+    if len(result) > 0:
+        result[0]['entries'].append({'type':'start','time': str(now.time())})
+        db.insert(result[0])
+    else:
+        db.insert({'day':str(now.date()), 'entries':[{'type':'start','time': str(now.time())}]})
+    return run("started!")
+
+def stop():
+    now = datetime.datetime.now()
+    entry = Query()
+    result = db.search(entry.day == str(now.date()))
+    if len(result) > 0:
+        result[0]['entries'].append({'type':'end','time': str(now.time())})
+        db.insert(result[0])
+        return info("stopped")
+    else:
+        return bad("no daily entry found")
 
 def main(args=sys.argv[1:]):
     """
@@ -28,10 +57,15 @@ def main(args=sys.argv[1:]):
 
     if len(args) == 0 : print(usage())
 
-    print(args)
+    if include_debug_info :
+        print(orange(str(Path.home().joinpath("jobdiary.json"))))
 
-    if include_debug_info:
-        if args == ['--debug']:
-            return
 
+    commands = {
+    "start": start,
+    "stop": stop
+    }
+
+    func = commands.get(args[0], lambda: "Invalid command")
+    print(func())
     return
